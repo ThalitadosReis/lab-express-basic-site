81d3924ea3c8c22f11dc8d5a5856d26d9bea96e8
Thalita dos Reis <dosreistha@gmail.com>
2022-12-13 18:15:36 +0100

    latest set up

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ const app = express();
 
 app.use(express.static('public'));
 
-//this is the homepage route
-app.get("/", (request, response, next)=>{
+// this is the homepage route
+app.get("/", (request, response, next) =>{
     response.sendFile(__dirname + '/views/home.html');
 })
 
-//this is the about route
-app.get("/about", (request, response, next)=>{
+// this is the about route
+app.get("/about", (request, response, next) =>{
     response.sendFile(__dirname + '/views/about.html');
 })
 
-//this is the work route
-app.get("/work", (request, response, next)=>{
+// this is the work route
+app.get("/work", (request, response, next) =>{
     response.sendFile(__dirname + '/views/work.html');
 })
 
-//this is the bonus route
-app.get("/bonus", (request, response, next)=>{
+// this is the bonus route
+app.get("/bonus", (request, response, next) =>{
     response.sendFile(__dirname + '/views/bonus.html');
 })
+
 app.listen(3000)
