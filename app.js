10ece474b14730541c356fd4fcac0719c18beb60
Thalita dos Reis <dosreistha@gmail.com>
2022-12-13 17:47:08 +0100

    basic instructions set up built

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+//this is the homepage route
+app.get("/", (request, response, next)=>{
+    response.sendFile(__dirname + '/views/home.html');
+})
+
+//this is the about route
+app.get("/about", (request, response, next)=>{
+    response.sendFile(__dirname + '/views/about.html');
+})
+
+//this is the work route
+app.get("/work", (request, response, next)=>{
+    response.sendFile(__dirname + '/views/work.html');
+})
+
+//this is the bonus route
+app.get("/bonus", (request, response, next)=>{
+    response.sendFile(__dirname + '/views/bonus.html');
+})
+app.listen(3000)
